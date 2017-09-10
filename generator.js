01dae891295c736c7ac82a718768c7cefd614e33
Wickramaranga Abeygunawardhana <umstek@live.com>
2017-09-10 08:01:39 +0530

    Add more code and travis config

--- generator.js
+++ generator.js
@@ -2,8 +2,8 @@ import Chance from 'chance';
 const chance = new Chance();
 
 /**
- * Goes through an array to generate contents. 
- * @param {!Array.<(string|object)>} a 
+ * Traverse through an array to generate contents. 
+ * @param {!Array.<(string|object|Array)>} a 
  */
 function traverseArray(a) {
     if (a === undefined) {
@@ -23,4 +23,23 @@ function traverseArray(a) {
     }
 }
 
-export { traverseArray };
+/**
+ * Traverse through an object to generate contents. 
+ * @param {!Object.<string, (string|object|Array)>} o 
+ */
+function traverseObject(o) {
+    if (o === undefined) {
+        throw new Error("Object argument must be provided.");
+    }
+
+    if (o === null) {
+        throw new Error("Object argument must not be null.");
+    }
+
+    if (o.constructor !== {}.constructor) {
+        throw new Error("Only an object can be provided.")
+    }
+
+}
+
+export { traverseArray, traverseObject };
--- test/generator.js
+++ test/generator.js
@@ -1,6 +1,6 @@
 import { expect } from 'chai';
 
-import { traverseArray } from '../generator';
+import { traverseArray, traverseObject } from '../generator';
 
 describe('generator', () => {
     describe('.traverseArray', () => {
@@ -10,9 +10,9 @@ describe('generator', () => {
 
         it('should throw if array is null', () => {
             expect(() => traverseArray(null)).to.throw();
-        }); 
+        });
 
-        it('should throw if not an array', () => {
+        it('should throw if argument is not an array', () => {
             expect(() => traverseArray(true)).to.throw();
             expect(() => traverseArray(1.1)).to.throw();
             expect(() => traverseArray("array")).to.throw();
@@ -27,5 +27,31 @@ describe('generator', () => {
         it('should throw if array contains anything except objects or strings');
 
         it('should return same number of items');
+    });
+
+    describe(".traverseObject", () => {
+        it('should throw if no object is provided', () => {
+            expect(traverseObject).to.throw();
+        });
+
+        it('should throw if object is null', () => {
+            expect(() => traverseObject(null)).to.throw();
+        });
+
+        it('should throw if argument is not an object', () => {
+            expect(() => traverseObject(true)).to.throw();
+            expect(() => traverseObject(1.1)).to.throw();
+            expect(() => traverseObject("array")).to.throw();
+            expect(() => traverseObject(new Symbol())).to.throw();
+            expect(() => traverseObject([])).to.throw();
+        });
+
+        it('should return empty object for an empty object')
+
+        it('should throw if any object key is not a string');
+
+        it('should throw if any object value is not array or object or string');
+
+        it('should return an object with the same number of entries');
     });
 });
